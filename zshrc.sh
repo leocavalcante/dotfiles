0ad1fa688c74a1f2ccc5f8635d9fab47adcd432b
leocavalcante <lc@leocavalcante.com>
2022-04-27 20:34:30 -0300

    tweaks for macos

--- zshrc.sh
+++ zshrc.sh
@@ -5,7 +5,16 @@
 export ZSH="$HOME/.oh-my-zsh"
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
-export PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --with-zlib --with-readline --with-gettext --with-sodium --with-ffi --enable-zts"
+export PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --with-zlib --with-readline --with-iconv --with-gettext --with-sodium --with-ffi --enable-zts"
+
+# macOS :rolleyes:
+if [[ $(uname -a) =~ "Darwin" ]]; then
+    alias nproc="sysctl -n hw.ncpu"
+
+    export PHP_CONFIGURE_OPTIONS="$PHP_CONFIGURE_OPTIONS --with-iconv=$(brew --prefix libiconv)"
+    export PHP_CONFIGURE_OPTIONS="$PHP_CONFIGURE_OPTIONS --with-readline=$(brew --prefix readline)"
+    export PHP_CONFIGURE_OPTIONS="$PHP_CONFIGURE_OPTIONS --with-openssl=$(brew --prefix openssl)"
+fi
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
