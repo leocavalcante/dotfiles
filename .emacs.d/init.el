fd52a8af009ee1cb1c31bf06d5dcf90e3b9e8f49
leocavalcante <leonardo.cavalcante@picpay.com>
2023-04-25 00:21:17 -0300

    delete unused packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -98,7 +98,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(psysh yasnippet yaml use-package treemacs-projectile treemacs-magit multiple-cursors material-theme lsp-ui lsp-treemacs github-theme github-dark-vscode-theme flycheck-phpstan dockerfile-mode company-phpactor company-go auto-complete all-the-icons)))
+   '(psysh yasnippet yaml use-package treemacs-projectile treemacs-magit multiple-cursors github-dark-vscode-theme flycheck-phpstan dockerfile-mode company-phpactor company-go auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
