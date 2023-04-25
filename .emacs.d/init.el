f08e1ed3935725255282a2fabe62f7a0fc7f6d49
leocavalcante <leonardo.cavalcante@picpay.com>
2023-04-25 00:05:15 -0300

    reorganize emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,3 @@
-;;; init.el --- Emacs Init file
-
-;;; Commentary:
-;;
-;;; Code:
-
 (setq auto-save-default t
       ido-everywhere t
       make-backup-files nil
@@ -16,7 +10,6 @@
 (scroll-bar-mode -1)
 (show-paren-mode t)
 (tool-bar-mode -1)
-(yas-global-mode t)
 
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
@@ -26,46 +19,49 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
-(use-package github-dark-vscode-theme
-  :ensure t
-  :config (load-theme 'github-dark-vscode t))
+(use-package magit
+  :ensure t)
 
 (use-package company
   :ensure t
-  :init (setq company-minimum-prefix-length 0)
-  :config (global-company-mode t))
-
-(use-package dockerfile-mode
-  :ensure t)
-
-(use-package multiple-cursors
-  :ensure t
-  :bind (("C-c d" . mc/mark-next-like-this)
-	 ("C-c C-d" . mc/mark-all-like-this)))
-
-(use-package lsp-mode
-  :ensure t
-  :hook (php-mode . lsp))
-
-(use-package lsp-ui
-  :ensure t)
+  :init
+  (setq company-minimum-prefix-length 0)
+  :config
+  (global-company-mode t))
 
 (use-package flycheck
   :ensure t
-  :init (global-flycheck-mode)
+  :config
+  (global-flycheck-mode)
   :hook
   (flycheck-mode-hook . flycheck-highlighting-mode)
   (flycheck-mode-hook . flycheck-indication-mode))
 
-(use-package magit
-  :ensure t)
-
 (use-package projectile
   :ensure t
-  :init (projectile-mode +1)
+  :init
+  (projectile-mode +1)
   :bind
   (:map projectile-mode-map ("C-c p" . projectile-command-map))
   (:map projectile-mode-map ("C-c f" . projectile-find-file)))
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode t))
+
+(use-package lsp-mode
+  :ensure t
+  :hook
+  (php-mode . lsp))
+
+(use-package github-dark-vscode-theme
+  :ensure t
+  :config
+  (load-theme 'github-dark-vscode t))
+
+(use-package auto-complete
+  :ensure t)
 
 (use-package treemacs
   :ensure t
@@ -84,23 +80,10 @@
   :after (treemacs magit)
   :ensure t)
 
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-c d" . mc/mark-next-like-this)
+	 ("C-c C-d" . mc/mark-all-like-this)))
 
-;; customize (do not edit)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(github-dark-vscode-theme company-mode yasnippet treemacs-projectile treemacs-magit php-mode multiple-cursors lsp-ui lsp-treemacs flycheck company-phpactor)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(provide 'init)
-
-;;; init.el ends here
+(use-package dockerfile-mode
+  :ensure t)
