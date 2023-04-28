d87c92ac9855c78d71fc20455eb4c10ef63e872f
leocavalcante <leonardo.cavalcante@picpay.com>
2023-04-28 13:08:38 -0300

    stow ignore

--- .emacs.default/init.el
+++ .emacs.default/init.el
@@ -14,6 +14,7 @@
 (scroll-bar-mode -1)
 (show-paren-mode t)
 (tool-bar-mode -1)
+(load-theme 'misterioso t)
 
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
