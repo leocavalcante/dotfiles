b09e8f21e6b0755b4c9c477c29adb4216d4ba6f4
leocavalcante <lc@leocavalcante.com>
2024-05-01 02:58:09 -0300

    emacs old friend

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,40 @@
 (setq make-backup-files nil)
 (setq vc-follow-symlinks t)
+(setq visible-bell t)
+
 (menu-bar-mode -1)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(use-package go-mode
+  :ensure t
+  :config
+  (add-hook 'go-mode-hook 'eglot-ensure))
+
+(use-package eglot
+  :ensure t)
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode))
+
+(use-package yasnippet-snippets
+  :ensure t)
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(yasnippet-snippets)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
