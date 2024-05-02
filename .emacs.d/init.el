5275ff75161d1550650a1174ebec2075fdb3e3f6
leocavalcante <lc@leocavalcante.com>
2024-05-01 21:22:55 -0300

    ai

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,22 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (use-package emacs
   :init
   (setq make-backup-files nil)
@@ -11,6 +27,14 @@
   :config
   (menu-bar-mode -1)
   (add-hook 'prog-mode-hook 'display-line-numbers-mode))
+
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
+  :ensure t)
+
+(use-package gptel
+  :straight t
+  :ensure t)
 
 (use-package lsp-mode
   :ensure t)
