4ca3d8a95f04e13f7fee6a90ecba05b4beb4624e
leocavalcante <lc@leocavalcante.com>
2024-06-05 21:58:17 -0300

    lsp :tada:

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+       {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {'lua_ls', 'phpactor'},
+                handlers = {
+                    function(server_name)
+                         require('lspconfig')[server_name].setup({})
+                    end,
+                },
+            })
+        end
+    },
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+}
