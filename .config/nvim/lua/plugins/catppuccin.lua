2d164a0a5a8c3143d18779ac229a9e4e7aad1cad
leocavalcante <lc@leocavalcante.com>
2024-06-06 21:34:41 -0300

    fix catppuccin

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,14 +3,6 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        require('catppuccin').setup({
-            flavour = "mocha",
-            transparent_background = false,
-            integrations = {
-                treesitter = true
-            }
-        })
-
-        vim.cmd.colorscheme "catppuccin"
+        vim.cmd.colorscheme = "catppuccin"
     end
 }
