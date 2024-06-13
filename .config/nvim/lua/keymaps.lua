fff4638856a9b87e13654906048e6a072f2ec153
leocavalcante <leonardo.cavalcante@picpay.com>
2024-06-13 17:25:23 -0300

    feat(nvim): Using jj to exit insert mode

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,4 +2,4 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exit insert mode' });
