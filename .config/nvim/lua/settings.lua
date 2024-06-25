6ec8ecd96c72ea18c2c5306de74dce072af3c76e
leocavalcante <lc@leocavalcante.com>
2024-06-25 09:15:06 -0300

    refactor(nvim): Don't show mode

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -12,6 +12,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.shiftwidth = 4
+vim.opt.showmode = false
 vim.opt.smartcase = true
 vim.opt.softtabstop = 4
 vim.opt.splitbelow = true
