fdd39caebcdac2367340059f9f550f16411182b5
leocavalcante <leonardo.cavalcante@picpay.com>
2025-12-18 09:59:40 -0300

    feat: apply GitHub Dark theme to starship, lualine and delta
    - starship: add palette and style git, directory, character
    - lualine: custom theme using nvim colorscheme colors
    - delta: style diffs with matching add/remove colors
    
    Signed-off-by: leocavalcante <leonardo.cavalcante@picpay.com>

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,9 +2,36 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        -- GitHub Dark theme for lualine
+        local colors = require('colors.github-dark').colors
+        local github_dark = {
+            normal = {
+                a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
+                b = { fg = colors.fg, bg = colors.bg_highlight },
+                c = { fg = colors.fg_muted, bg = colors.bg },
+            },
+            insert = {
+                a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
+            },
+            visual = {
+                a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
+            },
+            replace = {
+                a = { fg = colors.bg, bg = colors.red, gui = 'bold' },
+            },
+            command = {
+                a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
+            },
+            inactive = {
+                a = { fg = colors.fg_muted, bg = colors.bg_highlight },
+                b = { fg = colors.fg_muted, bg = colors.bg_highlight },
+                c = { fg = colors.fg_muted, bg = colors.bg },
+            },
+        }
+
         require('lualine').setup({
             options = {
-                theme = 'auto'
+                theme = github_dark
             }
         })
     end
