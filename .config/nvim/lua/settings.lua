f4fb58eeffaf9bff463c1b3f9ccda3af4ca7e887
leocavalcante <leonardo.cavalcante@picpay.com>
2026-03-19 15:05:18 -0300

    fix(nvim): yaml folding

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -27,8 +27,7 @@ require("colors.github-dark").setup()
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "yaml",
   callback = function()
-    vim.opt_local.foldmethod = "expr"
-    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt_local.foldmethod = "indent"
     vim.opt_local.foldenable = true
     vim.opt_local.foldlevel = 99
   end,
