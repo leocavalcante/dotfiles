b4557a7ed3c2300dea93e73790fae8bc7fb4dcf5
leocavalcante <leonardo.cavalcante@picpay.com>
2026-03-20 09:04:00 -0300

    feat(nvim): add vsplit keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,3 +3,4 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exit insert mode' });
+vim.keymap.set('n', '<leader><C-l>', ':vsplit<CR>', { desc = 'Vertical split' })
