6df503841538ed0208601f1d87de35b7e979fedf
leocavalcante <leonardo.cavalcante@picpay.com>
2026-04-08 17:35:55 -0300

    fix(nvim): auto change directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,3 +9,12 @@ require('settings')
 require('keymaps')
 require('lazy').setup('plugins')
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+      vim.cmd("cd " .. vim.fn.fnameescape(arg))
+    end
+  end,
+})
+
