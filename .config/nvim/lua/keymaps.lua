e7d99688bf3ae21e99f215f9bf54c0b3febc6b0f
leocavalcante <leonardo.cavalcante@picpay.com>
2026-04-08 16:29:58 -0300

    feat(nvim): base64 keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,3 +4,17 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exit insert mode' });
 vim.keymap.set('n', '<leader><C-l>', ':vsplit<CR>', { desc = 'Vertical split' })
+
+vim.keymap.set('v', '<leader>64d', function()
+  vim.cmd('normal! "xy')
+  local decoded = vim.fn.system('base64 -d', vim.fn.getreg('x')):gsub('\n$', '')
+  vim.fn.setreg('x', decoded)
+  vim.cmd('normal! gv"xp')
+end, { desc = 'Decode base64 selection' })
+
+vim.keymap.set('v', '<leader>64e', function()
+  vim.cmd('normal! "xy')
+  local encoded = vim.fn.system('base64', vim.fn.getreg('x')):gsub('\n$', '')
+  vim.fn.setreg('x', encoded)
+  vim.cmd('normal! gv"xp')
+end, { desc = 'Encode base64 selection' })
