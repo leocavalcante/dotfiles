4c786b0cb5bc62d658e179c48edf680d6af860fd
leocavalcante <leonardo.cavalcante@picpay.com>
2026-05-01 11:31:45 -0300

    feat(nvim): open terminal in a floating window

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,32 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   keys = {
+    { "<leader>z", function()
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      if not _ZSH_TERM then
+        _ZSH_TERM = Terminal:new({
+          cmd = "zsh",
+          dir = vim.fn.getcwd(),
+          direction = "float",
+          float_opts = {
+            border = "curved",
+          },
+          close_on_exit = true,
+          hidden = true,
+          env = { TMUX = vim.env.TMUX, TMUX_PANE = vim.env.TMUX_PANE },
+          on_open = function(term)
+            vim.api.nvim_buf_set_keymap(term.bufnr, "t", "jj", [[<C-\><C-n>]], { noremap = true, silent = true })
+          end,
+          on_exit = function(term)
+            term:close()
+            _ZSH_TERM = nil
+          end,
+        })
+      end
+
+      _ZSH_TERM:toggle()
+    end, desc = "Zsh" },
     { "<leader>ico", function()
       local Terminal = require("toggleterm.terminal").Terminal
 
